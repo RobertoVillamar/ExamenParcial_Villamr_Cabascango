cd9c59a0e7b5c074c74bbe49f2aeb30800c9bc8f
MilenaEC <milena.cabascango695@ist17dejulio.edu.ec>
2024-12-12 17:02:08 -0500

    Creacion libro

---
+++ src/modelo/Libro.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Libro {
+
+    private Autor autor;
+
+    private String titulo;
+    private int agnioLanzamiento;
+    private int cantidadLibros;
+    
+    
+    public Libro(){
+        
+    }
+
+    public Libro(Autor autor, String titulo, int agnioLanzamiento, int cantidadLibros) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.agnioLanzamiento = agnioLanzamiento;
+        this.cantidadLibros = cantidadLibros;
+    }
+
+    public Autor getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAgnioLanzamiento() {
+        return agnioLanzamiento;
+    }
+
+    public void setAgnioLanzamiento(int agnioLanzamiento) {
+        this.agnioLanzamiento = agnioLanzamiento;
+    }
+
+    public int getCantidadLibros() {
+        return cantidadLibros;
+    }
+
+    public void setCantidadLibros(int cantidadLibros) {
+        this.cantidadLibros = cantidadLibros;
+    }
+    
+    
+    
+    
+    
+
+}
+ 
